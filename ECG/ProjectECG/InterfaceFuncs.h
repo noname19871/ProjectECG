5353e127bef404fae163de834ffac8195c47420b
ValarValar <dan4eg98@mail.ru>
2017-04-05 01:12:17 +0400

    Remake data input to a  file

--- ECG/ProjectECG/InterfaceFuncs.h
+++ ECG/ProjectECG/InterfaceFuncs.h
@@ -11,10 +11,17 @@
 // size - count of TextBoxes
 std::vector<double> ParseDatasIntoDoubleVector(System::Windows::Forms::TableLayoutPanel^ p, int size);
 
+//It resets textboxes in TableLayoutPanel^ p to value "";
+void ResetTextboxesInDataTable(System::Windows::Forms::TableLayoutPanel^ p);
+
+
+
+void SaveInFile(std::string filename, std::string diversion_name, std::vector<double> v, std::string vector_name);
+
 void PrepareFile(std::string filename);
 
 //It saves values from vector to csv file 
-void SaveWavesToFile(std::vector<double> v, std::string filename, std::string vector_name);
+void SaveWavesToFile(std::vector<double> v, std::string filename, std::string vector_name, int pos);
 
 //It draws grid for ECG graphic
 void DrawGrid(System::Drawing::Graphics^ g, int width, int height);
